b2059cfc7d7bd8274dceaaee56330c9e1c7515b3
CayoLopes <cayocesarlopes@gmail.com>
2023-09-21 00:32:40 -0300

    Telas do menu produtos criadas

--- produtos.c
+++ produtos.c
@@ -29,3 +29,105 @@ void sub_menu_produto(void){
     scanf("%c", &op);
     getchar();
 }
+
+char produto_cads(){
+  char op;
+    clearScreen();
+    printf("\n");
+
+    printf("********************************************************************************* \n"); 
+    printf("********************  P R O D U T O S  C A D A S T R A D O S  ******************* \n");
+    printf("********************************************************************************* \n");
+    printf("**                                                                             ** \n");
+    printf("**                 Nome:                                                       ** \n");
+    printf("**                 Código:                                                     ** \n");
+    printf("**                 Estoque:                                                    ** \n");
+    printf("**                                                                             ** \n");
+    printf("**                 Nome:                                                       ** \n");
+    printf("**                 Código:                                                     ** \n");
+    printf("**                 Estoque:                                                    ** \n");
+    printf("**                                                                             ** \n");
+    printf("**                 Nome:                                                       ** \n");
+    printf("**                 Código:                                                     ** \n");
+    printf("**                 Estoque:                                                    ** \n");
+    printf("**                                                                             ** \n");
+    printf("********************************************************************************* \n");
+    printf("\n");
+    scanf("%c", &op);
+    getchar();
+
+  
+}
+
+char cadas_produto(){
+    char op;
+    clearScreen();
+    printf("\n");
+
+    printf("********************************************************************************* \n"); 
+    printf("**********************  C A D A S T R A R  P R O D U T O S  ********************* \n");
+    printf("********************************************************************************* \n");
+    printf("**                                                                             ** \n");
+    printf("**                 Digite o nome:                                              ** \n");
+    printf("**                 Digite o código:                                            ** \n");
+    printf("**                 Digite o preço:                                             ** \n");
+    printf("**                 Digite o estoque:                                           ** \n");
+    printf("**                 Digite a descrição:                                         ** \n");
+    printf("**                                                                             ** \n");
+    printf("********************************************************************************* \n");
+    printf("\n");
+    scanf("%c", &op);
+    getchar();
+
+  
+
+
+}
+
+char info_produto(){
+    char op;
+    clearScreen();
+    printf("\n");
+
+    printf("********************************************************************************* \n"); 
+    printf("*******************  I N F O R M A C O E S  D O  P R O D U T O  ***************** \n");
+    printf("********************************************************************************* \n");
+    printf("**                                                                             ** \n");
+    printf("**                 nome:                                                       ** \n");
+    printf("**                 Código:                                                     ** \n");
+    printf("**                 Preço:                                                      ** \n");
+    printf("**                 Estoque:                                                    ** \n");
+    printf("**                 Descrição:                                                  ** \n");
+    printf("**                                                                             ** \n");
+    printf("********************************************************************************* \n");
+    printf("\n");
+    scanf("%c", &op);
+    getchar();
+
+  
+
+
+}
+
+
+char edit_produto(){
+    char op;
+    clearScreen();
+    printf("\n");
+
+    printf("********************************************************************************* \n"); 
+    printf("*************************  E D I T A R  P R O D U T O  ************************* \n");
+    printf("********************************************************************************* \n");
+    printf("**                                                                             ** \n");
+    printf("**                 Novo nome:                                                  ** \n");
+    printf("**                 Novo código:                                                ** \n");
+    printf("**                 Novo preço:                                                 ** \n");
+    printf("**                 Novo estoque:                                               ** \n");
+    printf("**                 Nova descrição:                                             ** \n");
+    printf("**                                                                             ** \n");
+    printf("********************************************************************************* \n");
+    printf("\n");
+    scanf("%c", &op);
+    getchar();
+}
+
--- produtos.h
+++ produtos.h
@@ -1,1 +1,5 @@
-void sub_menu_produto(void);
+void sub_menu_produto(void); //Menu dos produtos
+char produto_cads(); //Exibe os produtos que já foram cadastrados
+char cadas_produto(); //Faz o cadastro de novos produtos
+char info_produto(); //Mostra as informações de um produto
+char edit_produto(); //Faz a edição do produto
